d86c34aa87107228d4277d5e20f198ace878eaf4
Oksana Harr <oksana.harris@gmail.com>
2017-04-24 19:41:26 -1000

    finished

--- js/app.js
+++ js/app.js
@@ -6,15 +6,37 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent(){
+  var pElemOne = document.getElementById("more");
+  pElemOne.innerHTML = bacon;
+
+}
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+function lessContent (){
+  var lessClick = document.getElementById("less");
+  //lessClick.innerHTML = "";
+  lessClick.style.visibility = "hidden";
+
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
+
+function zoom (){
+  var biggieP = document.getElementById("biggie");
+  biggieP.style.fontSize = "150%";
+}
+
+function unzoom (){
+  var biggieP = document.getElementById("biggie");
+  biggieP.style.fontSize = "100%";
+}
+
 
 
 //4. McDonalds
@@ -22,13 +44,30 @@ The function will hide the contents in the pargraph with the id of `less` after 
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+var menuItems = ["fries","shake", "pies"];
+
+function valueMenu(){
+  //var menuP = document.getElementById("menu");
+  menu.innerHTML = menuItems.join(", ");
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 20px after clicking on the text.*/
 
+function redFace(){
+  var pElemRF = document.getElementById("drink");
+  pElemRF.style.fontSize = "24px";
+  pElemRF.style.color = "red";
+}
+
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
+
+function showPrice(){
+  var priceElem = document.getElementById("price");
+  priceElem.innerHTML="$5.55";
+}
 
 
 //7. Mr. Buttons
@@ -36,14 +75,44 @@ Next, create a function named valueMenu that will display your favorite items in
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+var quoteButton = document.getElementById("Benjamin");
+quoteButton.addEventListener("click", displayQt);
+
+function displayQt(){
+  var quoteElem = document.getElementById("displayQuote");
+  quoteElem.innerHTML = myQuote;
+}
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+var randomButton = document.getElementById("random");
+randomButton.addEventListener("click", randomQuoteFn);
 
+function randomQuoteFn(){
+  var pElemRQ = document.getElementById("displayQuotes");
+  var chosenQuote = quotes[Math.floor(Math.random()*quotes.length)];
+  pElemRQ.innerHTML = chosenQuote;
+}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clickig on the button.
 */
+
+// var secretButton = document.getElementById("showHide");
+// secretButton.addEventListener("click", getSecret);
+
+// function getSecret(){
+//   var hiddenElem = document.getElementById("showmoney");
+//   if (hiddenElem.style.display === "none"){
+//     hiddenElem.style.display = "block";
+//   } else {
+//     hiddenElem.style.display = "none";
+//   }
+// }
+
+$("#showHide").click(function getSecretJQ() {
+  $("#showmoney").toggle();
+});
