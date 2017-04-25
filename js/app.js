a97c9097d754aea70d5220e5de74559514f94037
Oksana Harr <oksana.harris@gmail.com>
2017-04-24 19:57:08 -1000

    improved

--- js/app.js
+++ js/app.js
@@ -75,12 +75,10 @@ function showPrice(){
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
-var quoteButton = document.getElementById("Benjamin");
-quoteButton.addEventListener("click", displayQt);
+Benjamin.addEventListener("click", displayQt);
 
 function displayQt(){
-  var quoteElem = document.getElementById("displayQuote");
-  quoteElem.innerHTML = myQuote;
+  displayQuote.innerHTML = myQuote;
 }
 
 //8. Say It again, Randomly
@@ -88,31 +86,32 @@ function displayQt(){
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
-var randomButton = document.getElementById("random");
-randomButton.addEventListener("click", randomQuoteFn);
+random.addEventListener("click", randomQuoteFn);
 
 function randomQuoteFn(){
-  var pElemRQ = document.getElementById("displayQuotes");
   var chosenQuote = quotes[Math.floor(Math.random()*quotes.length)];
-  pElemRQ.innerHTML = chosenQuote;
+  displayQuotes.innerHTML = chosenQuote;
 }
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clickig on the button.
 */
 
-// var secretButton = document.getElementById("showHide");
-// secretButton.addEventListener("click", getSecret);
+var secretButton = document.getElementById("showHide");
+secretButton.addEventListener("click", getSecret);
+var counter = 0;
 
-// function getSecret(){
-//   var hiddenElem = document.getElementById("showmoney");
-//   if (hiddenElem.style.display === "none"){
-//     hiddenElem.style.display = "block";
-//   } else {
-//     hiddenElem.style.display = "none";
-//   }
-// }
+function getSecret(){
+  var hiddenElem = document.getElementById("showmoney");
+  if (counter%2 === 0){
+    hiddenElem.style.display = "block";
+  } else {
+    hiddenElem.style.display = "none";
+  }
+  counter++;
+  console.log(counter);
+}
 
-$("#showHide").click(function getSecretJQ() {
-  $("#showmoney").toggle();
-});
+// $("#showHide").click(function getSecretJQ() {
+//   $("#showmoney").toggle();
+// });
